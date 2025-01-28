8f8f55f1536af36b6838d6eb00e7d626793c4f10
d.gremm <d.gremm@gbs-solutions.de>
2025-01-28 13:20:24 +0100

    Added webserver and canvas functionality

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import Express from 'express';
+
 import { EActivationFunction } from './lib/neuronal-net/activation-functions';
 import NeuronalNet from './lib/neuronal-net/neuronal-net';
 
@@ -9,3 +11,13 @@ const nn = new NeuronalNet({
 });
 
 nn.send([1, 0, 2]);
+
+const app = Express();
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
